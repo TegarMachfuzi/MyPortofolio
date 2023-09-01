d61662243790a1324e48405ca3da9ab8c81036ea
Tegar Machfudzi <v-tegarmachfudzi@anteraja.id>
2023-09-01 17:03:04 +0700

    adding dependency react-scroll and change logic scroll navbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,8 +11,8 @@ const Contact = () => {
         <div className='flex justify-center items-center'>
           <form action="" className='flex flex-col w-full md:w-1/2'>
             <input type="text" name='name' placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-            <input type="text" name='name' placeholder='Enter your mail' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-            <textarea name="message" rows="10" className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
+            <input type="text" name='name' placeholder='Enter your mail' className=' my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
+            <textarea name="message" placeholder='Enter your message' rows="10" className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
 
             <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's Talk</button>
           </form>
