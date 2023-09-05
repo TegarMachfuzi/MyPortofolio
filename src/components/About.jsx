cb634078b9f12c83ae496713b22cc9ede35421ea
Tegar Machfudzi <v-tegarmachfudzi@anteraja.id>
2023-09-05 09:19:36 +0700

    edit page about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,22 +9,13 @@ const About = () => {
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
 
-        <p className='text-xl mt-20'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Ex sint aperiam optio accusantium velit a, voluptates est in ipsa beatae, exercitationem,
-            minima reiciendis. Fugiat praesentium dolor saepe ut, error aspernatur. 
-            Ipsam pariatur illum tempora sed ipsa magnam distinctio totam minima ea voluptates,
-            natus dolorem qui nesciunt veritatis provident a iure?
+        <p className='text-xl mt-20'>Hello, I'm Tegar Machfudzi, I'm a Java developer.Over 1 years of working experience in developing Java Backend. I am good at working with a team. I can understand work quickly and can adapt quickly
         </p>
 
         <br />
 
         <p className='text-xl'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ad aspernatur illum, 
-            dolorum blanditiis ex distinctio dignissimos omnis repellendus? 
-            Aliquid ratione sequi voluptate officiis nobis veniam et maxime commodi, 
-            reprehenderit, harum illo ipsa eius. 
-            Eos assumenda odio incidunt vitae a commodi dolores, id saepe, ullam quos consectetur 
-            adipisci nemo perspiciatis.
+        Where I work now I work as a backend developer. I work following the direction of the product owner in the team. the job of a backend developer is to provide data or stream data so that it can be displayed or retrieved by the android developer team.
         </p>
       </div>
     </div>
